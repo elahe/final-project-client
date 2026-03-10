ab7b77b5cd511dbce927b0869b29a6dfd03983d4
Elahe <elahe6474@gmai.com>
2026-03-10 14:23:46 +0100

    add product in app.jsx to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
      <Routes>
-       <Route path="/" element={<HomePage/>}/>
+       <Route path="/" element={<HomePage products={products}/>}/>
        <Route path="/signup" element={<Signup/>}/>
        <Route path="/login" element={<Login/>}/>
        <Route path="/productes" element={<ProductPage/>}/>
